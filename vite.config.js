6f13308695cc0a34e368279dcc4b0f80d913e16f
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-04-14 16:13:55 +0600

    update vite confige file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import tailwindcss from '@tailwindcss/vite'
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+import { defineConfig } from 'vite';
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
+  },
   plugins: [
     tailwindcss(),
   ],
