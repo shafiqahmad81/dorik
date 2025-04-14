081b438bde5553adcf3ca4949da7d45c6eb6e2af
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-04-14 23:47:50 +0600

    complete dorik project

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
 import { defineConfig } from 'vite';
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        
-      },
-    },
-  },
   plugins: [
     tailwindcss(),
   ],
