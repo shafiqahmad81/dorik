974a9e7c163dd9188b5944b3e053280a275876d8
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-04-15 19:50:38 +0600

    update responsive menu in javascript

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 import './style.css';
 
+const hamburgerIcon = document.getElementById('hamburger-icon');
+const closeIcon = document.getElementById('close-icon');
+const mobileMenu = document.getElementById('menu')
+
+hamburgerIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden')
+    closeIcon.classList.toggle('hidden')
+    hamburgerIcon.classList.toggle('hidden')
+})
+closeIcon.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden')
+    hamburgerIcon.classList.toggle('hidden')
+    closeIcon.classList.toggle('hidden')
+})
